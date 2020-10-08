d3fea4c56b8450b6986fc316c4619018d61f52cf
RomainRETIF <62280459+RomainRETIF@users.noreply.github.com>
2020-10-08 16:37:10 +0200

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -11,14 +11,7 @@ if(isset($_GET['btnconnexion']) && $_GET['txtlogin'] != "i" && $_GET['txtlogin']
         CURLOPT_TIMEOUT => 30,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_POSTFIELDS => "
-        {
-        filters : [{
-        column_name : login,
-        filter_predicate : contains,
-        filter_term : $_GET[txtlogin],
-        filter_term_modifier : }]
-        }",
+        CURLOPT_POSTFIELDS => "{\n\t\"filters\" : \n\t[{\n\t\t\"column_name\":\"login\",\n\t\t\n\t\t\"filter_predicate\":\"contains\",\n\t\t\n\t\t\"filter_term\":\"$_GET[txtlogin]\",\n\t\t\n\t\t\"filter_term_modifier\": \"\"\n\t}]\n}",
         CURLOPT_HTTPHEADER => array(
             "authorization: Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDIzNTY2MDgsImR0YWJsZV91dWlkIjoiMTAwNzUyOTc4ZmEzNDYwMzkyMjZkYmU1NGE2YTNhNmEiLCJ1c2VybmFtZSI6ImJmODBlMTE5YzczYzQ5YjU4MTBhMGUxNjliNDEwZGU4QGF1dGgubG9jYWwiLCJwZXJtaXNzaW9uIjoicncifQ.NKAke-X7-ICr1gulCb-rf9w68ZjgvOdNZPPZQWXB-y4",
             "content-type: application/json"
@@ -32,8 +25,10 @@ if(isset($_GET['btnconnexion']) && $_GET['txtlogin'] != "i" && $_GET['txtlogin']
 
     if ($err) {
         echo "cURL Error #:" . $err;
-    } else {
+    }
+    else {
         $affichage = json_decode($response, true);
+
         if($affichage['rows'][0]['prof_id'] != "")
             echo $affichage['rows'][0]['Prenom'];
         else
