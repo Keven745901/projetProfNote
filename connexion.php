3b3b77265b800bf99238aa4de7d43d8bae201e18
RomainRETIF <62280459+RomainRETIF@users.noreply.github.com>
2020-10-08 16:13:19 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,43 @@
+<?php
+
+if(isset($_GET['btnconnexion']) && $_GET['txtlogin'] != "i" && $_GET['txtlogin'] != "I") {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://cloud.seatable.io/dtable-server/api/v1/dtables/100752978fa346039226dbe54a6a3a6a/filtered-rows/?table_name=Professeur",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => "
+        {
+        filters : [{
+        column_name : login,
+        filter_predicate : contains,
+        filter_term : $_GET[txtlogin],
+        filter_term_modifier : }]
+        }",
+        CURLOPT_HTTPHEADER => array(
+            "authorization: Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDIzNTY2MDgsImR0YWJsZV91dWlkIjoiMTAwNzUyOTc4ZmEzNDYwMzkyMjZkYmU1NGE2YTNhNmEiLCJ1c2VybmFtZSI6ImJmODBlMTE5YzczYzQ5YjU4MTBhMGUxNjliNDEwZGU4QGF1dGgubG9jYWwiLCJwZXJtaXNzaW9uIjoicncifQ.NKAke-X7-ICr1gulCb-rf9w68ZjgvOdNZPPZQWXB-y4",
+            "content-type: application/json"
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        $affichage = json_decode($response, true);
+        if($affichage['rows'][0]['prof_id'] != "")
+            echo $affichage['rows'][0]['Prenom'];
+        else
+            echo "Login ou mot de passe invalide(s).";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+</head>
+<body>
+    <form method="GET" action="index.php">
+        <input type="text" name="txtlogin" placeholder="Login" required="required">
+        <br>
+        <input type="password" name="txtmdp" placeholder="Mot de passe" required="required">
+        <br><br>
+        <input type="submit" name="btnconnexion" value="Connexion">
+        <br><br>
+        <?php require 'connexion.php' ?>
+    </form>
+</body>
+</html>
