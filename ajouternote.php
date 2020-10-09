c09ffc3f656eb476b884a448919d2ea6c9ca7af8
anguinea <58850607+anguinea@users.noreply.github.com>
2020-10-09 20:38:33 +0200

    Add files via upload
    Note ajoutée via un formulaire, manque le lien avec 1 élève

--- ajouternote.php
+++ ajouternote.php
@@ -1,10 +1,39 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Note</title>
+</head>
+<body>
+    <form method="GET" action="ajouternote.php">
+        <input type="text" name="valeur" placeholder="valeur" required="required">
+        <br>
+        <input type="text" name="coefficient" placeholder="coefficient" required="required">
+        <br><br>
+        <input type="submit" name="ajouter" value="Ajouter">
+        <br><br>
+    </form>
+</body>
+</html>
+
 <?php
+
+if(!isset($_GET['valeur']) && !isset($_GET['coefficient'])){
+	echo "Entrez les infos svp";
+}
+
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://cloud.seatable.io/dtable-server/api/v1/dtables/100752978fa346039226dbe54a6a3a6a/rows/');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n\n\t\"row\": {\"valeur\": \"12\" , \"coefficient\": \"3\"},\n\n\t\"table_name\": \"Note\"\n\n}");
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+	"{
+		\"row\": 
+			{\"valeur\": \"$_GET[valeur]\",
+			\"coefficient\": \"$_GET[coefficient]\"},
+		\"table_name\": \"Note\"
+	}"
+);
 
 $headers = array();
 $headers[] = 'Authorization: Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDI0ODU2MTMsImR0YWJsZV91dWlkIjoiMTAwNzUyOTc4ZmEzNDYwMzkyMjZkYmU1NGE2YTNhNmEiLCJ1c2VybmFtZSI6ImJmODBlMTE5YzczYzQ5YjU4MTBhMGUxNjliNDEwZGU4QGF1dGgubG9jYWwiLCJwZXJtaXNzaW9uIjoicncifQ.V3oceMKDhZUt1cupOkOl_C4L5kQNRSrJqWr7SraA89Q';
@@ -14,7 +43,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
 ?>
