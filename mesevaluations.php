6848da1a0d5ecf38b30a1c2d19607605ffc8c73a
RomainRETIF <62280459+RomainRETIF@users.noreply.github.com>
2020-10-12 09:34:30 +0200

    Add files via upload

--- mesevaluations.php
+++ mesevaluations.php
@@ -4,14 +4,12 @@ require "fonctions.php";
 if(isset($_SESSION['professeur_id']))
 {
     $evals = reponseFiltree('Evaluation','professeur_id',$_SESSION['professeur_id'], 'contains');
-    
-    echo "<form method='GET' action='evaluation.php'>";
+
     if(isset($evals['rows'][0]['evaluation_id'])) {
         foreach ($evals['rows'] as $item) {
             echo "<a href='evaluation.php?eval=$item[evaluation_id]&libelle=$item[libelle]&coefficient=$item[coefficient]'>" . $item['libelle'] . "</a> " . $item['evaluation_date'] . " <a style='color:red' href='supprimereval.php?eval=$item[_id]'>Supprimer</a>";
             echo "<br>";
         }
-        echo "</form>";
     }
     else
         echo "Pas d'évaluations à afficher.";
