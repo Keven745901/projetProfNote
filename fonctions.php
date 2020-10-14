a966e71aff5f46b4d7b1946cd08f97bcd79ac28c
RomainRETIF <62280459+RomainRETIF@users.noreply.github.com>
2020-10-14 11:13:36 +0200

    Add files via upload

--- fonctions.php
+++ fonctions.php
@@ -290,4 +290,48 @@ function supprimerNotes()
     
 }
 
+function authentification($login,$mdp)
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://cloud.seatable.io/dtable-server/api/v1/dtables/100752978fa346039226dbe54a6a3a6a/filtered-rows/?table_name=Professeur",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POSTFIELDS => "{\"filters\" : 
+            [{\"column_name\":\"login\",
+            \"filter_predicate\":\"is\",
+            \"filter_term\":\"" . $login . "\",
+            \"filter_term_modifier\": \"\"},
+            
+            {\"column_name\":\"mdp\",
+            \"filter_predicate\":\"is\",
+            \"filter_term\":\"" . $mdp . "\",
+            \"filter_term_modifier\": \"\"}
+            ],
+            \"filter_conjunction\":\"And\"
+            }",
+        CURLOPT_HTTPHEADER => array(
+            "authorization: Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDI2ODIxOTgsImR0YWJsZV91dWlkIjoiMTAwNzUyOTc4ZmEzNDYwMzkyMjZkYmU1NGE2YTNhNmEiLCJ1c2VybmFtZSI6ImJmODBlMTE5YzczYzQ5YjU4MTBhMGUxNjliNDEwZGU4QGF1dGgubG9jYWwiLCJwZXJtaXNzaW9uIjoicncifQ.0W4G8Y841meSF5prFFZIXArC6wUPHfeizVaF3-xlQI4",
+            "content-type: application/json"
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        $affichage = json_decode($response, true);
+        return $affichage;
+    }
+}
+
 ?>
